161bf8c5d36627519cb0548bf23b0b3e6b85fc58
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-08 14:03:57 -0400

    finished up to step 7

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -3,10 +3,19 @@ import React from 'react';
 const FilmListing = ({films}) => {
 const filmTitles = films.map(film => {
     return (
-    <h1 key={film.id}>{film.title}</h1>
+        <div className="film-row" key={film.id}>
+            <figure className="film-poster">
+            <img src={`https://image.tmdb.org/t/p/w780${film.poster_path}`} alt="" />
+            </figure>
+    
+            <div className="film-summary">
+            <h1>{film.title}</h1>
+            <p>{new Date(film.release_date).getFullYear()}</p>
+            </div>
+        </div>
     )
 })
-    console.log(filmTitles)
+
     return (
         <div className="film-list">
           <h1 className="section-title">FILMS</h1>
